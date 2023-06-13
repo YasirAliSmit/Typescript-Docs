5e49b233a614f195683522fb118b4dcf51b242cd
YasirAliSmit <yasir.ali75404@gmail.com>
2023-06-13 16:31:37 +0500

    learn

---
+++ Demo.ts
@@ -0,0 +1,41 @@
+//Typscript is a Super set of JavaScript
+
+//There are Two Way to Type a code in typescript
+
+//Number No:1 is implicite
+
+//Number No:2 is explicite
+
+//This is implicite Way of Write a Code
+//Here is Example of this
+// var implicite = 'implicite'
+
+// Here is Example of Write of Explicite Type of Writing a Code
+//Here is example of Explicite way
+//var userInfo:string ="Yasir"
+
+//There are Example of Simple Type
+// var userInfo :string = 'Yasir' ;
+// var userNumber :number = 21 ;
+// var userMale :boolean = true
+
+//There are Two More Types Spacial
+
+// var number:any = '03243375002' ;
+// number = 23 ;
+
+//Here is Example of Any Type in TypeScripte in JavaScript TypeScript
+//In Any U can asign any data type in Variable leter on U can change
+// var userInfo :any = 'string'
+// userInfo  = 23
+// userInfo  = true
+
+//In This Example in side any u can not assign with number
+//var phoneNumber :number[] = [30,24,24,45]
+
+// var genderMale : boolean[] = [true,false]
+
+// Let tolk about read only Type
+//Here is Example
+//const content: readonly [] = ['Yasir']
+//iN this example u can read only data not mutate this data its means u can not change.
